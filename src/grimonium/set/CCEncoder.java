ea2425f9853f89668de7705f60dc82b99654870c
Michael Forrest <michael.forrest@gmail.com>
2009-01-06 00:52:27 +0000

    made relative encoders work

--- src/grimonium/set/CCEncoder.java
+++ src/grimonium/set/CCEncoder.java
@@ -1,10 +1,11 @@
 package grimonium.set;
 
+import grimonium.Ableton;
+import grimonium.GroupElement;
 import microkontrol.MicroKontrol;
 import microkontrol.controls.Encoder;
 import microkontrol.controls.EncoderListener;
 import processing.xml.XMLElement;
-import grimonium.GroupElement;
 
 public class CCEncoder extends GroupElement implements EncoderListener {
 	private Encoder encoder;
@@ -14,13 +15,14 @@ public class CCEncoder extends GroupElement implements EncoderListener {
 	//<encoder id="7" name="tempo" channel="1" cc="2"/>
 	public CCEncoder(XMLElement element) {
 		cc = element.getIntAttribute("cc");
-		channel = element.getIntAttribute("channel");
+		channel = element.getIntAttribute("channel")-1;
 		encoder = MicroKontrol.getInstance().encoders[element.getIntAttribute("id")];
 		encoder.listen(this);
 	}
 
 	public void moved(Integer change) {
-
+		int message = (change > 0) ? change: 64 - change;
+		Ableton.sendCC(channel,cc,message);
 	}
 
 }
