5457ebe4b95ff840bf0c8e31fc0a384c26c576e2
Michael Forrest <michael.forrest@gmail.com>
2009-01-25 20:55:16 +0000

    removed all animata maps

--- src/grimonium/maps/ElementFactory.java
+++ src/grimonium/maps/ElementFactory.java
@@ -15,16 +15,6 @@ public class ElementFactory {
 
 		if(name.equals("multilayerfader")) return new MultiLayerFader(element);
 
-
-		// related to animata, depracated
-		if(name.equals("notebone")) return new NoteBone(element);
-		if(name.equals("faderbone")) return new FaderBone(element);
-		if(name.equals("freqbone")) return new FreqBone(element);
-		if(name.equals("noterangebone")) return new NoteRangeBone(element);
-		if(name.equals("bonetempokeys")) return new BoneTempoKeys(element);
-		if(name.equals("bonetempokeyranges")) return new BoneTempoKeyRanges(element);
-		//
-
 		System.out.println("Error parsing element " + name);
 		return null;
 	}
